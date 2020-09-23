bf1d99ec6e10bcea9626c638b09387721cccc8f5
baonghi1201 <44896400+baonghi1201@users.noreply.github.com>
2020-09-23 11:50:07 +0300

    Node, callbacks and error handling

--- node_example/index.js
+++ node_example/index.js
@@ -2,14 +2,19 @@ var rect = require('./rectangle.js');
 
 function solveRect(a,b){
     console.log('Solving rectangle with a= ' + a + " and b=" + b);
-    if (a<=0 || b<=0){
-        console.log ('The value of "a" or "b" must be greater than 0')
-    }
-    else{
-        console.log("The perimeter of the rectangle is " + rect.perimeter(a,b))
-        console.log("The area of the rectangle is " + rect.area(a,b));
-    }
+
+    rect(a,b,(err,rectangle)=>{
+        if(err){
+            console.log('Error', err.message);
+        }
+        else{
+            console.log('Area of the rectangle is.......... '+ rectangle.area());
+            console.log('Perimeter of the rectangle is..... '+ rectangle.perimeter());
+        }
+    });
+    console.log("It takes 2 seconds for 'rect()' to process ");
 }
 
 solveRect(3,5);
+solveRect(0,-2);
 
