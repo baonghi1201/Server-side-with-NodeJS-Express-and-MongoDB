73244852942fed4c4d6cd8af70cc8dff2fbae53a
baonghi1201 <44896400+baonghi1201@users.noreply.github.com>
2020-10-09 13:44:30 +0300

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,15 +8,14 @@ const connect = mongoose.connect(url);
 connect.then((db)=>{
     console.log('Connected correctly to the server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name:'Veggie Pizza',
         description:'Made out of vegetable & fruit'
-    });
-
-    newDish.save()
+    })
     .then((dish)=>{
         console.log(dish);
-        return Dishes.find({});
+
+        return Dishes.find({}).exec();
     })
     .then((dishes)=>{
         console.log(dishes);
--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -2,6 +2,10 @@ const mongoose=require('mongoose');
 
 const Schema=mongoose.Schema;
 
+const commentSchema= new Schema({
+    
+});
+
 const dishSchema= new Schema({
     name:{
         type: String,
